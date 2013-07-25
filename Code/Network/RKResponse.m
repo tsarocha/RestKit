1cfd715c4ebfd7e1933700fccc7c5897f46b70ad
Sarocha Sothornprapakorn <tua.sarocha@gmail.com>
2013-07-25 16:09:28 +0700

    Fix EXC_BAD_ACCESS in RKResponse in case of an unreachable URL

--- Code/Network/RKResponse.m
+++ Code/Network/RKResponse.m
@@ -236,8 +236,10 @@ return __VA_ARGS__;                                                             
 {
     RKResponseIgnoreDelegateIfCancelled();
     _failureError = [error retain];
+    [_request retain];
     [_request invalidateTimeoutTimer];
     [_request didFailLoadWithError:_failureError];
+    [_request release];
 }
 
 - (NSInputStream *)connection:(NSURLConnection *)connection needNewBodyStream:(NSURLRequest *)request
